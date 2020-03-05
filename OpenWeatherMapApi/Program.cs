3a5b741bd3deb030ab03d3797e5f968ecc4ee1f6
DylanMasters <dy1anmasters93@gmail.com>
2020-03-04 21:22:32 -0600

    Wrote console messages to accept information to send to api.

--- OpenWeatherMapApi/Program.cs
+++ OpenWeatherMapApi/Program.cs
@@ -17,6 +17,16 @@ namespace OpenWeatherMapApi
 #endif
             var httpClient = new HttpClient();
 
+            Console.WriteLine("Please enter your Zip Code");
+            int zip = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your Country Code (examples.. US, CA, etc...)");
+            string countryCode = Console.ReadLine();
+
+            JObject jObject = JObject.Parse(key);
+            JToken token = jObject["ApiKey"];
+            string apiKey = token.ToString();
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather?zip={zip},{countryCode}&appid={apiKey}";
         }
     }
 }
