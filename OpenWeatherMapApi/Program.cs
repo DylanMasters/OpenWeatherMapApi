4db823e074e15e3a9d58a6af40167dee8ee869d1
DylanMasters <dy1anmasters93@gmail.com>
2020-03-04 21:08:01 -0600

    initial commit, added empty JSON files for key input.

---
+++ OpenWeatherMapApi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherMapApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG
+            string key = File.ReadAllText("appsettings.development.json");
+#else
+            string key = File.ReadAllText("appsettings.release.json");
+#endif
+            var httpClient = new HttpClient();
+
+        }
+    }
+}
